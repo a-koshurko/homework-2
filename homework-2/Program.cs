cc6e51f041053908f027cb47f74ad5949bdd3942
Andrii Koshurko <akos@simcorp.com>
2023-11-22 19:04:49 +0200

    assignment 1 & 2

---
+++ homework-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input Transaction Nominal:");
+            var transNominal = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input Trade Price:");
+            var tradePrice = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Is transaction type Buy or Sell?");
+            TransactionType trcType;
+            var userInput = Console.ReadLine();
+            trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
+
+            var currentValue = (int)trcType * transNominal * tradePrice;
+            Console.WriteLine($"Your Current value is {currentValue}!");
+
+            Console.WriteLine("Please input Original Price:");
+            var originalPrice = Int32.Parse(Console.ReadLine());
+
+
+            var factor = (trcType) = "-1" ? 1 : 0;
+            var profitLoss = (tradePrice - originalPrice) * transNominal;
+            Console.WriteLine($"Your Profit/Loss value is {profitLoss}!");
+
+        }
+    }
+}
---
+++ homework-2/TransactionType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2
+{
+    public enum TransactionType { Buy = 1, Sell = -1 }
+
+}
